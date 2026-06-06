b467a80c0a9af61467a3a845f864b8ee2712012c
Khristille Chico <kechico@up.edu.ph>
2026-06-06 13:23:29 +0800

    fix vite config error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  base: '/slingshot-game/',
+  plugins: [react()],
+  base: '/slingshot-game',
 })
